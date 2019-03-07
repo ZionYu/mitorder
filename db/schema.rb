907d313b653bd42c87bf9d230a0d1a5f4369cfdd
Zion <bone2057@gmail.com>
2019-03-07 09:55:20 +0800

    create order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190307015031) do
+ActiveRecord::Schema.define(version: 20190307015442) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
