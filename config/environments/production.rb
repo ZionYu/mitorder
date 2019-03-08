6e972f95416ed23ff0bd7acd75c759410103f37d
Zion <bone2057@gmail.com>
2019-03-08 13:35:17 +0800

    change mailer production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mitorder_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "https://mitorder.herokuapp.com" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
