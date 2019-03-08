177a262dee2722d85da497d75539e355426e5c31
Zion <bone2057@gmail.com>
2019-03-08 12:39:08 +0800

    add validations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,10 +9,14 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     if @order.products.map{|e| e.stock}.index(0).nil?
       @order.products.map{|e| e.stock-=1}
-      @order.save    
-      flash[:notice] = "購買成功，訂購明細已發送至您的Email"
-      UserMailer.notify_order_create(@order).deliver_now!
-      redirect_to orders_path
+      if @order.save    
+        flash[:notice] = "購買成功，訂購明細已發送至您的Email"
+        UserMailer.notify_order_create(@order).deliver_now!
+        redirect_to orders_path
+      else
+        flash[:alert] = "姓名、地址、Email不可空白"
+        redirect_to orders_path
+      end
     else
       flash[:alert] = "訂購失敗，選購的商品已售完"
       redirect_to orders_path
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
+  validates_presence_of :name, :email, :address
   has_and_belongs_to_many :products
 end
